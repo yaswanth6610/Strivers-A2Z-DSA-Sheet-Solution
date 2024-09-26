ac8cf2668d7bb8080860d0cce43e103fa9d03e79
Yaswanth Kotha <yaswanth.kotha2001@gmail.com>
2024-09-26 22:13:55 +0530

    setup input/ooutput.txt

---
+++ step1/lec1/Datatypes.java
@@ -0,0 +1,24 @@
+package step1.lec1;
+
+
+public class Datatypes {
+    public static void main(String[] args) {
+        
+
+        int a = 5;
+        long b = 12000000000000L;
+        float c = 4.5f;
+        double d = 34.00;
+        String name = "Yaswanth";
+        char ch = 'c';
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(name);
+        System.out.println(ch);
+
+        
+    }
+}
