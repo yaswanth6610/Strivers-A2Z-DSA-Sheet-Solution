755e41a1979a048ddff8f736703f673bf8522d95
Yaswanth Kotha <yaswanth.kotha2001@gmail.com>
2024-09-26 18:28:08 +0530

    user input and ouput code

--- step1/lec1/Input.java
+++ step1/lec1/Input.java
@@ -1,7 +1,22 @@
 package step1.lec1;
 
+import java.util.Scanner;
+
 public class Input {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        String  name = sc.next();
+        char intial = name.charAt(0);
+        int age = sc.nextInt();
+        float marks = sc.nextFloat();
+
+
+        System.out.println(name);
+        System.out.println(intial);
+        System.out.println(age);
+        System.out.println(marks);
+
+        sc.close();
     }
 }
